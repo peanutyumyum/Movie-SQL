1371f214e5a0ce8a3d5ecda699f39c0b549101d5
Jiwon <jrimit@gmail.com>
2021-12-09 00:06:25 +0900

    fix: branch_office bugs solved

--- movie/app/forms.py
+++ movie/app/forms.py
@@ -1,13 +1,1 @@
 from django import forms
-from .models import BranchOffice
-
-bucket = ()
-model = BranchOffice.objects.all()
-for i in model:
-    bucket += (i.name, i.name),
-
-
-# 지점을 선택할 수 있게 하는 select 태그를 구성된 form
-class BranchForm(forms.Form):
-    CHOICES = (bucket)
-    field = forms.ChoiceField(choices = CHOICES)
